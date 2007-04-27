7398a7e2eed451d79dcaac14e300a08c8d825cb5
msmits <none@none>
2007-04-27 18:37:26 +0000

    initial version of livetest.py is complete

--- imapclient/livetest.py
+++ imapclient/livetest.py
@@ -1,47 +1,20 @@
-import highimap
-import imaplib
+import sys
 from datetime import datetime
+from optparse import OptionParser
+import imapclient
 
-'''
-Docs:
-
-Must be used with an account that has an INBOX folder
-
-Each test function (test_...) is standalone but may use assume other methods or
-functions are working correctly.
-'''
-
-#XXX: test flags setting and checking
-#XXX: test with and without UID
-#XXX: integrate this into the main script?
-#XXX: decorators to pretty up test output
-
-def test_acls(server):
-    '''Test 
-    '''
-    print server.getacl('INBOX')
-    #XXX: test folder with spaces
-
-def setup_environment(server):
-    init_folder(server, 'some folder')
-    init_folder(server, 'INBOX')
-
-def init_folder(server, folder):
-    if not server.folder_exists(folder):
-        server.create_folder(folder)
-    clear_folder(server, folder)
-
-def clear_folder(server, folder):
-    server.select_folder(folder)
-    server.delete_messages(server.search())
-    server.expunge()
+#TODO: test search() more 
+#TODO: more fetch() testing
+#TODO: test error conditions
+#TODO: prettify test output
+#TODO: coverage checking
 
 def test_list_folders(server):
     folders = server.list_folders()
     assert len(folders) > 0, 'No folders visible on server'
     assert 'INBOX' in [f.upper() for f in folders], 'INBOX not returned'
-    #XXX: test wildcards
-    #XXX: test other folders...
+    #TODO: test wildcards
+    #TODO: test other folders...
 
 def test_select_and_close(server):
     num_msgs = server.select_folder('INBOX')
@@ -49,21 +22,35 @@ def test_select_and_close(server):
     assert num_msgs >= 0
     server.close_folder()
 
-def test_folder_exists(server):
+def test_folders(server):
+    '''Test folder manipulation
+    '''
     assert server.folder_exists('INBOX')
     assert not server.folder_exists('this is very unlikely to exist')
 
+    test_folder_name = 'test-%s' % datetime.now().ctime()
+
+    server.create_folder(test_folder_name)
+    assert server.folder_exists(test_folder_name)
+
+    server.select_folder(test_folder_name)
+    server.close_folder()
+
+    server.delete_folder(test_folder_name)
+    assert not server.folder_exists(test_folder_name)
+
 def test_append(server):
+    '''Test that appending a message works correctly
+    '''
     clear_folder(server, 'INBOX')
 
+    # Message time microseconds are set to 0 because the server may return
+    # time in with seconds precision.
+    msg_time = datetime.now().replace(microsecond=0)
+
     # Append message
-    msg_time = datetime.now()
-    resp = server.append(
-            'INBOX',
-            'Subject: something\n\nFoo',
-            ('abc',),
-            msg_time,
-            )
+    body = 'Subject: something\r\n\r\nFoo'
+    resp = server.append('INBOX', body, ('abc', 'def'), msg_time)
     assert isinstance(resp, str)
 
     # Retrieve the just added message and check that all looks well
@@ -78,32 +65,106 @@ def test_append(server):
     assert len(resp) == 1
     msginfo = resp.values()[0]
 
-    #XXX: not setting INTERNALDATE response when using Dovecot
+    # Time should match the time we specified
     assert msginfo['INTERNALDATE'] == msg_time
-    assert msginfo['FLAGS'] == ('abc',)
-    print msginfo['RFC822']
 
-def runtest(server):
-    #XXX: run all and report failures (decorator)
+    # Flags should be the same
+    assert 'abc' in msginfo['FLAGS']
+    assert 'def' in msginfo['FLAGS']
+
+    # Message body should match
+    assert msginfo['RFC822'] == body
+
+def test_flags(server):
+    '''Test flag manipulations
+    '''
+    server.select_folder('INBOX')
+    msgid = server.search()[0]
+
+    def _flagtest(func, args, expected_flags):
+        answer = func(msgid, *args)
+
+        assert answer.has_key(msgid)
+        answer_flags = answer[msgid]
+
+        # This is required because the order of the returned flags isn't
+        # guaranteed
+        answer_flags.sort()
+        expected_flags.sort()
+
+        assert answer_flags == expected_flags
+
+    base_flags = ['abc', 'def']
+    _flagtest(server.set_flags, [base_flags], base_flags)
+    _flagtest(server.get_flags, [], base_flags)
+    _flagtest(server.add_flags, ['boo'], base_flags + ['boo'])
+    _flagtest(server.remove_flags, ['boo'], base_flags)
+
+def runtests(server):
+    '''Run a sequence of tests against the IMAP server
+    '''
+    # The ordering of these tests is important
     test_list_folders(server)
     test_select_and_close(server)
-    test_folder_exists(server)
+    test_folders(server)
     test_append(server)
+    test_flags(server)
 
+def clear_folder(server, folder):
+    server.select_folder(folder)
+    server.delete_messages(server.search())
+    server.expunge()
+
+def command_line():
+    p = OptionParser()
+    p.add_option('-H', '--host', dest='host', action='store',
+            help='IMAP host connect to')
+    p.add_option('-P', '--port', dest='port', action='store',
+            default=143, help='IMAP port to use (default is 143)')
+    p.add_option('-u', '--username', dest='username', action='store',
+            help='Username to login with')
+    p.add_option('-p', '--password', dest='password', action='store',
+            help='Password to login with')
+    p.add_option('', '--clobber', dest='clobber', action='store_true',
+            default=False, help='These tests are destructive. Use this '
+            'option to bypass the confirmation prompt.')
+
+    options, args = p.parse_args()
+
+    if args:
+        p.error('unexpected arguments %s' % ' '.join(args))
+
+    for opt_name in ('host', 'username', 'password'):
+        if not getattr(options, opt_name):
+            p.error('%s must be specified' % opt_name)
+
+    return options
+
+def user_confirm():
+    print """\
+WARNING: These tests are destructive.
+Email in the specified account will be lost!
+"""
+    r = raw_input('Enter "yes" to confirm this is ok: ')
+    if r.lower() != 'yes':
+        print "Aborting tests."
+        sys.exit()
 
 def main():
-    server = highimap.HighIMAP4('127.0.0.1')
-    server.login('mailtest', 'foo')
+    options = command_line()
 
-    runtest(server)
+    if not options.clobber:
+        user_confirm()
 
-    #setup_environment(server)
-
-    #server.select_folder('INBOX')
-    #id = server.search()[0]
-
-    #print server.set_flags([id], ['boo', 'hoo'])
-    #print server.fetch([id], ['FLAGS'])
+    # Test with use_uid on and off
+    for use_uid in (True, False):
+        print '-'*60
+        print "Testing with use_uid=%r..." % use_uid
+        print '-'*60
+        server = imapclient.IMAPClient(options.host, use_uid=use_uid)
+        server.login(options.username, options.password)
+        runtests(server)
+        print 'SUCCESS'
 
 if __name__ == '__main__':
     main()
