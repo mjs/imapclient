0b1cd0c5a53ee4bcf66ef37b9f6dfc0d51c58d21
Menno Smits <menno@freshfoo.com>
2009-03-26 19:41:48 +0000

    Updated Copyright year to 2009

--- imapclient/imapclient.py
+++ imapclient/imapclient.py
@@ -12,7 +12,7 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
 
-# Copyright 2007 Menno Smits
+# Copyright 2009 Menno Smits
 
 import re
 import imaplib
