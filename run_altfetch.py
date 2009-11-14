41ca284dd061bdaefd589e8421bd470d0ba289ef
Menno Smits <menno@freshfoo.com>
2009-11-14 16:11:04 +0000

    mainified

--- run_altfetch.py
+++ run_altfetch.py
@@ -1,9 +1,14 @@
 from pprint import pprint
 from imapclient import IMAPClient
 
-i = IMAPClient('localhost')
-i.login('mailtest', 'foobar')
-i.select_folder('INBOX')
-msgs = i.search()
-print msgs
-pprint(i.altfetch(msgs[0], 'RFC822'))
+def main():
+    i = IMAPClient('localhost')
+    i.login('mailtest', 'foobar')
+    i.select_folder('INBOX')
+    msgs = i.search()
+    print msgs
+    i._imap.debug = 5
+    pprint(i.altfetch(msgs[0], 'RFC822'))
+
+if __name__ == '__main__':
+    main()
