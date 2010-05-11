81497ce8887eac97e280dc5153ab79d83738c50e
Menno Smits <menno@freshfoo.com>
2010-05-11 13:06:31 +0100

    Update copyright year

--- imapclient/test/test_response_parser.py
+++ imapclient/test/test_response_parser.py
@@ -12,7 +12,7 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
 
-# Copyright 2009 Menno Smits
+# Copyright 2010 Menno Smits
 
 '''
 Unit tests for the FetchTokeniser and FetchParser classes
