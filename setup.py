b63b8cbe83bcdb0a25e6d4fe4d36830d87f75df4
Menno Smits <menno@freshfoo.com>
2010-05-11 13:16:01 +0100

    Missed some copyright dates

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-# Copyright (c) 2009, Menno Smits
+# Copyright (c) 2010, Menno Smits
 # Released subject to the New BSD License
 # Please see http://en.wikipedia.org/wiki/BSD_licenses
 
