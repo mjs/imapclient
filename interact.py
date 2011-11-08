d245452ccfbb4a3eefe5e153e0a7fa1faf8eecae
Menno Smits <menno@freshfoo.com>
2011-11-08 22:12:38 +0000

    Simple driver scripts for interact and livetest

---
+++ interact.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python
+
+# Copyright (c) 2011, Menno Smits
+# Released subject to the New BSD License
+# Please see http://en.wikipedia.org/wiki/BSD_licenses
+
+from imapclient.interact import main
+main()
