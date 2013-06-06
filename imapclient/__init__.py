d44a048aac2f5aea2fb13475c7e3a31d6002da57
Menno Smits <menno@freshfoo.com>
2013-06-07 00:56:53 +0100

    Version bump and NEWS update for 0.10.1
    --HG--
    branch : stable

--- imapclient/__init__.py
+++ imapclient/__init__.py
@@ -7,7 +7,7 @@
 
 from __future__ import unicode_literals
 
-version_info = (0, 10, 0, 'final')
+version_info = (0, 10, 1, 'final')
 
 def _imapclient_version_string(vinfo):
     major, minor, micro, releaselevel = vinfo
