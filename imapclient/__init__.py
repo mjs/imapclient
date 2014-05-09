e5f7384ad7e22026230e14470ddc29fa17b0aa8a
Menno Smits <menno@freshfoo.com>
2014-05-09 17:34:07 +1200

    Version bump for 0.11

--- imapclient/__init__.py
+++ imapclient/__init__.py
@@ -7,7 +7,7 @@
 
 from __future__ import unicode_literals
 
-version_info = (0, 11, 0, 'alpha')
+version_info = (0, 11, 0, 'final')
 
 def _imapclient_version_string(vinfo):
     major, minor, micro, releaselevel = vinfo
