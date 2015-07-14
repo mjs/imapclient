8118e43de9539d73b49e166d8e975634e46896bb
Menno Smits <menno@freshfoo.com>
2015-07-14 17:09:40 +1200

    Missed copyright header

--- imapclient/config.py
+++ imapclient/config.py
@@ -1,3 +1,7 @@
+# Copyright (c) 2015, Menno Smits
+# Released subject to the New BSD License
+# Please see http://en.wikipedia.org/wiki/BSD_licenses
+
 from __future__ import unicode_literals
 
 try:
