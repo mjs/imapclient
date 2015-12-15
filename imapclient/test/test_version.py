0aa0d0c9c492978b76b425218cfd8cec0cd057d1
Menno Smits <menno@freshfoo.com>
2015-12-15 23:17:35 +1300

    Fix unit tests for version strings
    The tests were out of step with a recent change to always include the
    patch version in IMAPClient's version strings.
    
    --HG--
    branch : stable

--- imapclient/test/test_version.py
+++ imapclient/test/test_version.py
@@ -11,16 +11,16 @@ from .util import unittest
 class TestVersionString(unittest.TestCase):
 
     def test_dot_oh(self):
-        self.assertEqual(_imapclient_version_string((1, 0, 0, 'final')), '1.0')
+        self.assertEqual(_imapclient_version_string((1, 0, 0, 'final')), '1.0.0')
 
     def test_minor(self):
-        self.assertEqual(_imapclient_version_string((2, 1, 0, 'final')), '2.1')
+        self.assertEqual(_imapclient_version_string((2, 1, 0, 'final')), '2.1.0')
 
     def test_point_release(self):
         self.assertEqual(_imapclient_version_string((1, 2, 3, 'final')), '1.2.3')
 
     def test_alpha(self):
-        self.assertEqual(_imapclient_version_string((2, 1, 0, 'alpha')), '2.1-alpha')
+        self.assertEqual(_imapclient_version_string((2, 1, 0, 'alpha')), '2.1.0-alpha')
 
     def test_beta_point(self):
         self.assertEqual(_imapclient_version_string((2, 1, 3, 'beta')), '2.1.3-beta')
