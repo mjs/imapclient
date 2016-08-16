8d8cf6cf216725a0564b070331ed26904a838ff3
Menno Smits <menno@freshfoo.com>
2016-08-17 00:11:24 +1200

    Allow installation from alternate directories
    Installation used to fail if setup.py wasn't in the current working
    directory.
    
    Fixes #207.
    
    --HG--
    branch : stable

--- setup.py
+++ setup.py
@@ -20,7 +20,8 @@ IS_PY_26_OR_OLDER = MAJ_MIN <= (2, 6)
 IS_PY_34_OR_NEWER = MAJ_MIN >= (3, 4)
 
 # Read version info
-version_file = path.join('imapclient', 'version.py')
+here = path.dirname(__file__)
+version_file = path.join(here, 'imapclient', 'version.py')
 info = {}
 if IS_PY3:
     exec(open(version_file).read(), {}, info)
