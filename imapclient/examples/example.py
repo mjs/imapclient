be31a0cc3f2cfc69c58d156b233ab1cb07058717
Menno Smits <menno@freshfoo.com>
2017-08-08 13:15:11 +1200

    Fix non-Python 3 compatible example code
    Fixes #253.

--- imapclient/examples/example.py
+++ imapclient/examples/example.py
@@ -15,7 +15,7 @@ server = IMAPClient(HOST, use_uid=True, ssl=ssl)
 server.login(USERNAME, PASSWORD)
 
 select_info = server.select_folder('INBOX')
-print('%d messages in INBOX' % select_info['EXISTS'])
+print('%d messages in INBOX' % select_info[b'EXISTS'])
 
 messages = server.search(['NOT', 'DELETED'])
 print("%d messages that aren't deleted" % len(messages))
@@ -23,7 +23,7 @@ print("%d messages that aren't deleted" % len(messages))
 print()
 print("Messages:")
 response = server.fetch(messages, ['FLAGS', 'RFC822.SIZE'])
-for msgid, data in response.iteritems():
+for msgid, data in response.items():
     print('   ID %d: %d bytes, flags=%s' % (msgid,
                                             data[b'RFC822.SIZE'],
                                             data[b'FLAGS']))
