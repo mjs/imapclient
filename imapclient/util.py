bad16f776ec217bb5c914337667cd83545f844d8
Maxime Lorant <maxime.lorant@gmail.com>
2017-10-30 22:40:19 +0100

    Ignore invalid encoding in Address string representation
    Those strings can be found in malformed e-mails where strings have
    been encoded incorrectly. The original data can still be accessed
    through the Address namedtuple in case of debugging or if the user
    really wants to deal with the garbage.

--- imapclient/util.py
+++ imapclient/util.py
@@ -4,12 +4,22 @@
 
 from __future__ import unicode_literals
 
+import logging
 from six import binary_type, text_type
+
+logger = logging.getLogger(__name__)
 
 
 def to_unicode(s):
     if isinstance(s, binary_type):
-        return s.decode('ascii')
+        try:
+            return s.decode('ascii')
+        except UnicodeDecodeError:
+            logger.warning(
+                "An error occurred while decoding %s in ASCII 'strict' mode. Fallback to "
+                "'ignore' errors handling, some characters might have been stripped", s
+            )
+            return s.decode('ascii', 'ignore')
     return s
 
 
