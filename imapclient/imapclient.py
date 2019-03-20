8a9cc4959d105317b3d54baead4bd0cb4a9d7480
Carson Ip <carsonip@users.noreply.github.com>
2019-03-20 18:40:39 +0800

    Catch the right exception in folder_status (#371)
    * Catch the right exception in folder_status
    
    * Remove useless try-except in folder_status

--- imapclient/imapclient.py
+++ imapclient/imapclient.py
@@ -848,10 +848,7 @@ class IMAPClient(object):
         fname = self._normalise_folder(folder)
         data = self._command_and_check('status', fname, what_)
         response = parse_response(data)
-        try:
-            status_items = response[-1]
-        except ValueError:
-            raise Exception('folder_status ValueError: Input ' + str(response))
+        status_items = response[-1]
         return dict(as_pairs(status_items))
 
     def close_folder(self):
