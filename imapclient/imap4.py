5b1f5fd35fc9ad7d87fd5c059023335fadf4377e
Menno Finlay-Smits <inbox@menno.io>
2020-08-02 23:41:51 +1200

    Ensure timeout is used on Python 2.7
    _create_socket isn't used with the Python 2 version of imaplib so the
    open method has been overrided to make it consistent across Python
    version.
    
    Fixes #380.

--- imapclient/imap4.py
+++ imapclient/imap4.py
@@ -12,5 +12,12 @@ class IMAP4WithTimeout(imaplib.IMAP4):
         self._timeout = timeout
         imaplib.IMAP4.__init__(self, address, port)
 
+    def open(self, host='', port=143):
+        # This is overridden to make it consistent across Python versions.
+        self.host = host
+        self.port = port
+        self.sock = self._create_socket()
+        self.file = self.sock.makefile('rb')
+
     def _create_socket(self):
         return socket.create_connection((self.host, self.port), self._timeout.connect)
