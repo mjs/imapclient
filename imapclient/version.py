4eae213a153be9e653f4225055606e3aee8261a3
Menno Finlay-Smits <inbox@menno.io>
2022-07-09 13:51:44 +1200

    Bump version numbers for 2.3.0

--- imapclient/version.py
+++ imapclient/version.py
@@ -1,10 +1,10 @@
-# Copyright (c) 2018, Menno Smits
+# Copyright (c) 2022, Menno Smits
 # Released subject to the New BSD License
 # Please see http://en.wikipedia.org/wiki/BSD_licenses
 
 from __future__ import unicode_literals
 
-version_info = (2, 2, 0, "final")
+version_info = (2, 3, 0, "final")
 
 
 def _imapclient_version_string(vinfo):
