mjs/imapclient

commit 92255e96cd555cbf105e6a007c473aeb1ad2d343

chore: setup `flake8`, fix issues, & add to CI
  * Setup `flake8` as a tox environment
  * Fix issues flagged by `flake8`
  * Add running of `flake8` in the CI

imapclient/config.py
@@ -71,8 +71,11 @@ def get_string_config_defaults():
 
 
 def _read_config_section(parser, section):
-    get = lambda name: parser.get(section, name)
-    getboolean = lambda name: parser.getboolean(section, name)
+    def get(section, name):
+        return parser.get(section, name)
+
+    def getboolean(section, name):
+        return parser.getboolean(section, name)
 
     def get_allowing_none(name, typefunc):
         try:
@@ -195,7 +198,7 @@ def create_client_from_config(conf, login=True):
             assert conf.password, "missing password"
             client.login(conf.username, conf.password)
         return client
-    except:
+    except:  # noqa: E722
         client.shutdown()
         raise
 

.github/workflows/lint.yml
@@ -28,3 +28,5 @@ jobs:
       - run: pip install --upgrade tox
       - name: Run black code formatter (https://black.readthedocs.io/en/stable/)
         run: tox -e black -- --check
+      - name: Run flake8 (https://flake8.pycqa.org/en/latest/)
+        run: tox -e flake8

imapclient/__init__.py
@@ -5,7 +5,9 @@
 # version_info provides the version number in programmer friendly way.
 # The 4th part will be either alpha, beta or final.
 
-from .imapclient import *
-from .response_parser import *
-from .tls import *
-from .version import author as __author__, version as __version__, version_info
+from .imapclient import *  # noqa: F401,F403
+from .response_parser import *  # noqa: F401,F403
+from .tls import *  # noqa: F401,F403
+from .version import author as __author__  # noqa: F401
+from .version import version as __version__  # noqa: F401
+from .version import version_info  # noqa: F401

imapclient/imapclient.py
@@ -24,11 +24,9 @@ from .imap_utf7 import encode as encode_utf7, decode as decode_utf7
 from .response_parser import parse_response, parse_message_list, parse_fetch_response
 from .util import to_bytes, to_unicode, assert_imap_protocol, chunk
 
-try:
-    from select import poll
-
+if hasattr(select, "poll"):
     POLL_SUPPORT = True
-except:
+else:
     # Fallback to select() on systems that don't support poll()
     POLL_SUPPORT = False
 
@@ -1770,7 +1768,7 @@ class IMAPClient(object):
     def _normalise_labels(self, labels):
         if isinstance(labels, (str, bytes)):
             labels = (labels,)
-        return [_quote(encode_utf7(l)) for l in labels]
+        return [_quote(encode_utf7(label)) for label in labels]
 
     @property
     def welcome(self):

setup.py
@@ -4,7 +4,6 @@
 # Released subject to the New BSD License
 # Please see http://en.wikipedia.org/wiki/BSD_licenses
 
-import sys
 from os import path
 
 from setuptools import setup

tests/test_auth.py
@@ -2,7 +2,6 @@
 # Released subject to the New BSD License
 # Please see http://en.wikipedia.org/wiki/BSD_licenses
 
-from imapclient import IMAPClient
 from imapclient.exceptions import LoginError
 from .imapclient_test import IMAPClientTest
 

tests/test_enable.py
@@ -4,7 +4,6 @@
 
 from unittest.mock import Mock
 
-from imapclient import IMAPClient
 from imapclient.exceptions import IllegalStateError
 from .imapclient_test import IMAPClientTest
 

tests/test_util_functions.py
@@ -12,7 +12,6 @@ from imapclient.imapclient import (
     seq_to_parenstr,
     seq_to_parenstr_upper,
     _quoted,
-    _parse_untagged_response,
 )
 from imapclient.util import assert_imap_protocol
 

commit ec53de3bd11264333bf7ba736c6a4f187463467c

fix: broken `get()` & `getboolean()`
Commit 92255e96cd555cbf105e6a007c473aeb1ad2d343 introduced a bug by
adding `section` to the argument list, but the callers don't send
`section`.

Fix this by removing `section` from the argument list.

imapclient/config.py
@@ -71,10 +71,10 @@ def get_string_config_defaults():
 
 
 def _read_config_section(parser, section):
-    def get(section, name):
+    def get(name):
         return parser.get(section, name)
 
-    def getboolean(section, name):
+    def getboolean(name):
         return parser.getboolean(section, name)
 
     def get_allowing_none(name, typefunc):